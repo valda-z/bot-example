13ff7c76fd0841d5311def254b6e77dea482d948
valda-z <valdemar.zavadsky@microsoft.com>
2016-12-09 10:19:55 +0100

    new dialog dialect

--- app.js
+++ app.js
@@ -18,86 +18,96 @@ var connector = new builder.ChatConnector({
 //Create bot and add dialogs
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', new builder.IntentDialog()    
-.matches('/^weather/i',  [function (session) {
-    session.beginDialog('/weather');
-    }])
-    // The IntentDialog lets you add a RegEx that, when matched, 
-    //will invoke a Dialog Handler.
-.onDefault([
-    //The first step of the root ‘/’ dialog checks to see if we know 
-    //the user's location, and if not, it redirects them to the ‘/weather’ dialog
-    //using a call to beginDialog(). 
-    function (session, args, next) 
-    {
-        if (!session.userData.location) { session.beginDialog('/weather'); }
-        else { next(); }    
-            //You can persist data for a user globally by assigning values 
-            //to the session.userData object, like we've done here for location.
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change/i, [
+    function (session) {
+        session.beginDialog('/weather');
     },
-    function (session, results) 
-    {
-        if (session.userData.location != null) {
-            session.send('Hello from %s', session.userData.location + "!");
-        }
+    function (session, results) {
     }
-]));
+]);
+
+intents.matches(/^forecast/i, [
+    function (session) {
+        weather(session);
+    },
+    function (session, results) {
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.location) {
+            session.beginDialog('/weather');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send("Hello from city %s", session.userData.location + "!");
+        session.send("  if you want to see weather forecast enter 'forecast'");
+        session.send("  if you want to change city enter 'change'");
+    }
+]);
 
 bot.dialog('/weather', [
-    function (session, args, next) 
-    {
+    function (session) {
         builder.Prompts.text(session, "Hello. I can tell you about any city " +
-            "if you type it like 'weather London, UK'.");
-        //check to see if user is requesting a new location.
-        if (session.userData.location) { next(); }
+                "if you type it like 'London, UK'.");
     },
-    function (session, results)     //WeatherUnderground API 
-    {
-        session.send("Requesting city weather conditions ...");
-        try 
-        {   //Try to read in a string of "weather City, ST"
-            var txt = session.message.text;
-            //convert "Weather" to "weather", then delete it
-            txt = txt.toLowerCase().replace('weather ', '');
-            //split City, State by ‘,’ and replace spaces with _ 
-            var city = txt.split(',')[0].trim().replace(' ', '_');
-            //assign state variable to the back half of the string 
-            var state = txt.split(',')[1].trim();
-            //log City, ST to the console for debugging 
-            console.log(city + ', ' + state);
-            //set user's global location to City, ST 
-            session.userData.location = (city + ', ' + state.toUpperCase());
-
-            //Try to parse the City and State into a URL string
-            var url = '/api/' + wundergroundKey + '/conditions/q/state/city.json'
-            url = url.replace('state', state);
-            url = url.replace('city', city);
-            console.log('/.../' + state.toUpperCase() + '/' + city + '.json');
-
-           //Need to have "var http = require('http');" up top, and "npm install --save http"
-            http.get(
-                {
-                    host: 'api.wunderground.com',
-                    path: url
-                }, function (response) {
-                    var body = '';
-                    response.on('data', function (d) { body += d; })
-                    response.on('end', function () {
-                        var data = JSON.parse(body);
-                        var conditions = data.current_observation.weather;
-                        session.send("'" + conditions + "' in "
-                            + city + " right now, and the temperature is "
-                            + data.current_observation.temp_c + " degrees C.   "
-                            + data.current_observation.observation_time);
-                    });
-                })
-        } //End of try 
-        catch (e) 
-        { session.send("Whoops, that didn't match! Try again."); }
-
+    function (session, results) {
+        session.userData.location = results.response;
+        weather(session);
         session.endDialog();
-    } //End of WeatherUnderground API function 
-]); //End of ‘/weather’ dialog waterfall 
+    }
+]);
+
+function weather(session) {
+    try {   //Try to read in a string of "weather City, ST"
+        var txt = session.userData.location;
+        //convert "Weather" to "weather", then delete it
+        txt = txt.toLowerCase();
+        //split City, State by ‘,’ and replace spaces with _ 
+        var city = txt.split(',')[0].trim().replace(' ', '_');
+        //assign state variable to the back half of the string 
+        var state = txt.split(',')[1].trim();
+        //log City, ST to the console for debugging 
+        console.log(city + ', ' + state);
+        //set user's global location to City, ST 
+        session.userData.location = (city + ', ' + state.toUpperCase());
+
+        session.send("Requesting city weather conditions ...");
+
+        //Try to parse the City and State into a URL string
+        var url = '/api/' + wundergroundKey + '/conditions/q/state/city.json'
+        url = url.replace('state', state);
+        url = url.replace('city', city);
+        console.log('/.../' + state.toUpperCase() + '/' + city + '.json');
+
+        //Need to have "var http = require('http');" up top, and "npm install --save http"
+        http.get(
+            {
+                host: 'api.wunderground.com',
+                path: url
+            }, function (response) {
+                var body = '';
+                response.on('data', function (d) { body += d; })
+                response.on('end', function () {
+                    var data = JSON.parse(body);
+                    var conditions = data.current_observation.weather;
+                    session.send("'" + conditions + "' in "
+                        + city + " right now, and the temperature is "
+                        + data.current_observation.temp_c + " degrees C.   "
+                        + data.current_observation.observation_time);
+                });
+            })
+    } //End of try 
+    catch (e)
+    { session.send("Whoops, that didn't match! Try again."); }
+
+}
 
 //Waterfall dialogs allow any results returned from the first function 
 //to be passed as input to the next function.
@@ -113,10 +123,6 @@ var server = restify.createServer();
 server.get('/', restify.serveStatic({
     directory: __dirname,
     default: '/index.html'
-}));
-server.get('/img.svg', restify.serveStatic({
-    directory: __dirname,
-    default: '/img.svg'
 }));
 server.post('/api/messages', connector.listen());
 server.listen(process.env.PORT || 3000, function () {
